28650e23ab62dde4eaa338375833d717b6c568bc
shsxssp72 <shsxssp72@hotmail.com>
2020-03-26 12:39:54 +0800

    Add cors middleware

---
+++ AchieveItDependencies/AchieveItDependencies/CorsMiddleware.py
@@ -0,0 +1,12 @@
+class CorsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Credentials"] = "true"
+        response["Access-Control-Allow-Methods"] = "*"
+        response["Access-Control-Max-Age"] = "3600"
+        response["Access-Control-Allow-Headers"] = "content-type, authorization"
+        return response
--- AchieveItDependencies/AchieveItDependencies/settings.py
+++ AchieveItDependencies/AchieveItDependencies/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'AchieveItDependencies.AchieveItDependencies.apps.AchieveItPersonnelConfig',
+    'AchieveItDependencies.CorsMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,10 +44,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'AchieveItDependencies.CorsMiddleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'AchieveItDependencies.urls'
